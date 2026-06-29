84cba2910fb646ab1a0f8ba3bb6b252c8c712c89
wee08 <manutnithyasean@gmail.com>
2026-06-29 12:47:53 +0700

    timeout vs immediate

--- week1/Even_loop/event_loop_2.js
+++ week1/Even_loop/event_loop_2.js
@@ -1,7 +1,19 @@
+import fs from "node:fs";
+
 // timeout vs immediate
 setTimeout(() => {
   console.log("timeout");
 }, 0);
 setImmediate(() => {
   console.log("Immediate");
+});
+
+// timeout vs immediate
+fs.readFile(import.meta.filename, () => {
+  setTimeout(() => {
+    console.log("timeout");
+  }, 0);
+  setImmediate(() => {
+    console.log("immediate");
+  });
 });
