f9358dbac11df4ee879c53178470ead0b3945bc2
wee08 <manutnithyasean@gmail.com>
2026-06-30 21:07:26 +0700

    Refactor event loop demonstration to clarify execution order of next tick, microtasks, and timers

--- week1/Even_loop/event_loop_2.js
+++ week1/Even_loop/event_loop_2.js
@@ -17,3 +17,19 @@ fs.readFile(import.meta.filename, () => {
     console.log("immediate");
   });
 });
+
+console.log("1. Start");
+
+// Next tick queue
+process.nextTick(() => console.log("2. Next tick"));
+
+// Microtask queue (Promise)
+Promise.resolve().then(() => console.log("3. Promise"));
+
+// Timer phase
+setTimeout(() => console.log("4. Timeout"), 0);
+
+// Check phase
+setImmediate(() => console.log("5. Immediate"));
+
+console.log("6. End");
