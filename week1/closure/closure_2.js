ebbc3ebbdf28fd2d94eca8a9b1c09e669cbe9e71
wee08 <manutnithyasean@gmail.com>
2026-07-03 10:46:20 +0700

    javascript closure

---
+++ week1/closure/closure_2.js
@@ -0,0 +1,24 @@
+function createGame() {
+  let score = 0;
+
+  function increaseScore(points) {
+    score += points;
+    console.log(`+${points}pts`);
+  }
+  function decreaseScore(points) {
+    score -= points;
+    console.log(`-${points}pts`);
+  }
+  function getScore() {
+    return score;
+  }
+
+  return { increaseScore, decreaseScore, getScore };
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+console.log(`The final score is ${game.getScore()}pts`);
