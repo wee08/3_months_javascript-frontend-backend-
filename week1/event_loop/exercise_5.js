67fda4b0314393ac975f4bb224e9a015235845e1
wee08 <manutnithyasean@gmail.com>
2026-07-03 10:16:09 +0700

    Add event loop demonstration with setTimeout, setImmediate, and nextTick

---
+++ week1/event_loop/exercise_5.js
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+console.log("start");
+
+setTimeout(() => console.log("SetTimeout"), 0);
+setImmediate(() => console.log("setImmediate"));
+process.nextTick(() => console.log("nextTick"));
+
+Promise.resolve().then(() => console.log("promise"));
+
+console.log("end");
+
+// output: start,end, promise, nextTick, setImmediate, setTimeout
+// nextTick jumps the queue ahead of even the promise microtask because JavaScript has two ties microtasks system. when the mian synchronous code finishes, node drains the nextTick queue before checking before the V8 Promise miecrotask queue
