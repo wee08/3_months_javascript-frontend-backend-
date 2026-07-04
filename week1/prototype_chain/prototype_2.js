f7a69c3f0dab5787ada05ae7cc43f578d4549d77
wee08 <manutnithyasean@gmail.com>
2026-07-04 10:43:31 +0700

    add custom prototype to Person constructor

--- week1/prototype_chain/prototype_2.js
+++ week1/prototype_chain/prototype_2.js
@@ -3,7 +3,10 @@ function Person(first, last, age, eyeColor) {
   this.lastname = last;
   this.age = age;
   this.eyeColor = eyeColor;
+
+  this.personality = "khmer";
 }
+
 const myFather = new Person("Jonh", "Doe", 60, "Brown");
 const myMother = new Person("San", "Rally", 40, "Green");
 
@@ -12,3 +15,16 @@ myMother.skin = "white";
 
 myMother.skin = "light brown";
 console.log(myMother.skin);
+
+// Person.height = 170; cannot add to constructor
+
+console.log(myMother);
+
+// add prototype to Person
+Person.prototype.name = function () {
+  return this.firstname + " " + this.lastname;
+};
+
+const myGirlfriend = new Person("Mey", "Kimheng", 20, "Light brown");
+
+console.log(`She name is ${myGirlfriend.name()}`);
