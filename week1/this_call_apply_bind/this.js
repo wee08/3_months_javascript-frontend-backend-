8b74f1ed19d8c47d5494e98055382d16785d6cdf
wee08 <manutnithyasean@gmail.com>
2026-07-06 10:11:25 +0700

    this alone

--- week1/this_call_apply_bind/this.js
+++ week1/this_call_apply_bind/this.js
@@ -7,14 +7,19 @@ const person = {
 };
 
 console.log(person.fullName());
-
+// this in defualt function
 function myFunction() {
   return this;
 }
 console.log(myFunction());
 
+// this in strict mode
 ("use strict");
 function myFunction1() {
   return this;
 }
 console.log(myFunction1());
+
+// this alone
+let x = this;
+console.log(x);
