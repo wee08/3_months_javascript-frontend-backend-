4152471c2a6a755744df0cf6ac3fc50fa6d21cb8
wee08 <manutnithyasean@gmail.com>
2026-07-06 10:06:44 +0700

    this in an Oobject method

---
+++ week1/this_call_apply_bind/this.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: "Sean",
+  lastName: "Manutnithya",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+console.log(person.fullName());
