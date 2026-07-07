c4a8b429a739faf9aab5207dbd3a544a59c0893a
wee08 <manutnithyasean@gmail.com>
2026-07-07 13:24:51 +0700

    try apply()

---
+++ week1/this_call_apply_bind/apply/apply.js
@@ -0,0 +1,5 @@
+const numbers = [51, -10, 2, 4, 6];
+const max = Math.max.apply(null, numbers);
+console.log(max);
+const min = Math.min.apply(null, numbers);
+console.log(min);
--- week1/this_call_apply_bind/call/call_with_argument.js
+++ week1/this_call_apply_bind/call/call_with_argument.js
@@ -25,3 +25,5 @@ function sayHello() {
 const person3 = { name: "joson" };
 console.log(sayHello.call(person3));
 // call() runs immediately, doesn't return resusable function
+// What is the main difference between call() and a normal function call?
+// the main difference is that a normal function call binds this to the object it is called on, while .call() allows you to manually pass and explicity set the this context. this lets you borrw method from one object and use them on completely different objects.
