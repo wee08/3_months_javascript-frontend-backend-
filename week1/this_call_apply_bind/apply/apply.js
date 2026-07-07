5e01e507a16519d0c0a60099f0cf6fbfa9db3367
wee08 <manutnithyasean@gmail.com>
2026-07-07 13:31:43 +0700

    applay() to append an array to another

--- week1/this_call_apply_bind/apply/apply.js
+++ week1/this_call_apply_bind/apply/apply.js
@@ -3,3 +3,22 @@ const max = Math.max.apply(null, numbers);
 console.log(max);
 const min = Math.min.apply(null, numbers);
 console.log(min);
+
+// syntax
+// apply(thisArg);
+// apply(thisArg, argsArray);
+
+// function wrapper() {
+//   return anotherFn.apply(null, arguments);
+// }
+// function wrapper(...args) {
+//   return anotherFn(...args);
+// }
+
+const array = ["a", "b"];
+const elements = [0, 1, 2];
+// array.push.apply(array, elements);
+console.log(array);
+
+array.push(...elements);
+console.log(array);
