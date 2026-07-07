12ca90d659e55c21bfcd7d37702010fc3598d4f0
wee08 <manutnithyasean@gmail.com>
2026-07-07 13:47:45 +0700

    call() vs apply()

---
+++ week1/this_call_apply_bind/apply/apply_2.js
@@ -0,0 +1,14 @@
+const person1 = { name: "Jonh" };
+const person2 = { name: "Doe" };
+const person3 = { name: "Ringo" };
+function greet(greeting) {
+  return greeting + " " + this.name;
+}
+// const greetMessages = ["whats up", "Hello", "Hi"];
+const greetMessages = ["whats up"];
+console.log(greet.apply(person3, greetMessages));
+
+// call vs apply
+
+console.log(greet.call(person1, "sup"));
+console.log(greet.apply(person1, ["sup"]));
