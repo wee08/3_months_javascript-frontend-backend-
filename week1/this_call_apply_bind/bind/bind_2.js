41f88e87e1c5e50d21270b86f07864a7edc14f8d
wee08 <manutnithyasean@gmail.com>
2026-07-08 11:00:05 +0700

    bind() with arguments

--- week1/this_call_apply_bind/bind/bind_2.js
+++ week1/this_call_apply_bind/bind/bind_2.js
@@ -10,3 +10,11 @@ person.display();
 console.log(person.display());
 
 setTimeout(person.display, 3000);
+
+// bind() with argument
+
+function multiply(a, b) {
+  return a * b;
+}
+const double = multiply.bind(null, 2);
+console.log(double(5));
