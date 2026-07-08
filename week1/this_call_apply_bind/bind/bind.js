2a4298978c2f4634735581b495c8494731e6fd8d
wee08 <manutnithyasean@gmail.com>
2026-07-08 10:41:46 +0700

    bind

---
+++ week1/this_call_apply_bind/bind/bind.js
@@ -0,0 +1,32 @@
+// const newFunction = functionName.bind(this,arg1,arg2,...);
+
+// using bind() to fix this
+const person1 = { name: "Jonh" };
+const person2 = { name: "Dara" };
+const person3 = { name: "Heng" };
+function greet() {
+  return "HELLO " + this.name;
+}
+const greetJonh = greet.bind(person1);
+greetJonh();
+console.log(greetJonh());
+
+// create person object
+const person = {
+  firstName: "Jonh",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+// create member object
+const member = {
+  firstName: "Hega",
+  lastName: "Nilsen",
+};
+// bind the fullName method to the memeber Object
+let fullName = person.fullName.bind(member);
+// call function name
+fullName();
+console.log(fullName());
