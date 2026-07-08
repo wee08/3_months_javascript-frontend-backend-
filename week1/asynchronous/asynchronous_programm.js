8143255740841afbb745c87b3c74666161deceb3
wee08 <manutnithyasean@gmail.com>
2026-07-08 11:22:55 +0700

    function order

--- week1/asynchronous/asynchronous_programm.js
+++ week1/asynchronous/asynchronous_programm.js
@@ -11,3 +11,18 @@ console.log(y);
 myDisplay("A");
 myDisplay("B");
 myDisplay("C");
+
+// control flow
+// run code base on order, top -> butom
+
+// function sequence
+// it runs in the sequence they are called, not the way they are defined
+
+function myFirst() {
+  myDisplay("Hello");
+}
+function mySecond() {
+  myDisplay("Goodbye");
+}
+myFirst();
+mySecond();
