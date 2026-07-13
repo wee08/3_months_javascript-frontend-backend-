07748868a60b8663188789ca59a91576bfdfdf6c
wee08 <manutnithyasean@gmail.com>
2026-07-13 16:16:57 +0700

    wating for a timeout

---
+++ week1/asynchronous/async_timeout.js
@@ -0,0 +1,6 @@
+// the setTimeout() method schedules a function to run after a delay in milliseconds. it is an async operation used to delay code execution withou freezing the browser
+
+setTimeout(myFunction, 3000);
+function myFunction() {
+  console.log("setTimeout");
+}
