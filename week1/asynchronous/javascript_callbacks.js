0948ac7b340da9824fece64973381d1c6ca178b8
wee08 <manutnithyasean@gmail.com>
2026-07-13 16:32:42 +0700

    async callback

--- week1/asynchronous/javascript_callbacks.js
+++ week1/asynchronous/javascript_callbacks.js
@@ -9,3 +9,14 @@ function display(message) {
   console.log(message);
 }
 greet("Jonh", display);
+// the display function is the callback
+// greet() function decides when to call it
+
+function calculate(x, y, operation) {
+  return operation(x, y);
+}
+function add(a, b) {
+  return a + b;
+}
+let result = calculate(3, 4, add);
+console.log(result);
