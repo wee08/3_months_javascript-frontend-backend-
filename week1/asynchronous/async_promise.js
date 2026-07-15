292cbc9930ddafb15a450f400d98f67981c5e81d
wee08 <manutnithyasean@gmail.com>
2026-07-15 11:15:04 +0700

    async promise

--- week1/asynchronous/async_promise.js
+++ week1/asynchronous/async_promise.js
@@ -1,10 +1,45 @@
-fetch("https://www.youtube.com/")
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// fetch("https://www.youtube.com/")
+//   .then(function (response) {
+//     return response.text();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+let myPromise = new Promise(function (resolve, reject) {
+  resolve("value");
+  reject("value");
+});
+// resolve function to run if finishes sucessfully
+// reject function to run if finishes with an error
+
+// using promise
+myPromise.then(
+  function (value) {
+    console.log("sucess");
+  },
+  function (value) {
+    console.log("fail");
+  },
+);
+
+let newPromise = new Promise(function (resolve, reject) {
+  let success = false;
+  if (success) {
+    resolve("done");
+  } else {
+    reject("failed");
+  }
+});
+
+myPromise.then(
+  function (value) {
+    console.log(value);
+  },
+  function (value) {
+    console.log(value);
+  },
+);
---
+++ week1/asynchronous/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+fetch("https://www.youtube.com/")
+//   .then(function (response) {
+//     return response.text();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
